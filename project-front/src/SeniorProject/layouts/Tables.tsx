295aa489f07db28ea10d6040268583dc954fdd2a
Randy Owens <randyowens2019@yahoo.com>
2023-04-18 21:09:46 -0400

    restore

--- project-front/src/SeniorProject/layouts/Tables.tsx
+++ project-front/src/SeniorProject/layouts/Tables.tsx
@@ -17,7 +17,7 @@ export default () => {
       const fetchTests = async () => {
 
          // URL to backend request - dont need any specification yet
-         const url: string = "https://senior-project-back:8080/api/tests";
+         const url: string = "https://senior-project-back.onrender.com/api/tests";
 
          // hold fetch response
          const response = await fetch(url);
